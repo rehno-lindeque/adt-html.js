87831af59893a36cb476ea249686542b17be29c1
Rehno Lindeque <errantkid@gmail.com>
2012-05-08 13:37:29 -0700

    Create a test that creates html dom elements and places them on the page

--- test/test.js
+++ test/test.js
@@ -1,5 +1,15 @@
 console.log("-- Test 1 (...) --");
 (function(){
   console.log("TODO");
+
+  var elResult = document.getElementsByClassName('reactive-result')[0];
+  while (elResult.firstChild) {
+    elResult.removeChild(elResult.firstChild);
+  }
+  elResult.appendChild((function(_){
+    _.h1("Heading #1"),
+    _.h2("Heading #2"),
+    _.p("A paragraph with some ", _.em("emphasized and ", _.strong("ocassionally strong")), " text")
+  })(html.eval));
 })();
 
