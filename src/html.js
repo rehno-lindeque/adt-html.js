4a0dc13375813e0e2a4d768ac0c470948d44d596
Rehno Lindeque <errantkid@gmail.com>
2012-05-08 13:38:08 -0700

    Add constructors for all html5 elements (a list of these were taken from http://joshduck.com/periodic-table.html)

--- src/html.js
+++ src/html.js
@@ -1,6 +1,32 @@
-  // Define a local copy of adt
+  // Using the html5 list of tags from (http://joshduck.com/periodic-table.html)
+  // Other html api's should probably be in separate libraries? (xhtml, html4 etc)
   var html = {
-    cons: adt('div', 'span', 'p'),
+    cons: adt(
+      // Root element
+      'html', 
+      // Metadata and scripting
+      'head','title','meta','base','link','style','noscript','script',
+      // Text-level semantics
+      'span','a','rt','rp','dfn','abbr','q','cite','em','time','var','samp','i',
+      'b','sub','sup','small','strong','mark','ruby','ins','del','bdi','bdo',
+      's','kbd','wbr','code',
+      // Grouping content
+      'br','hr','figcaption','figure','p','ol','ul','li','div','pre',
+      'blockquote','dl','dt','dd',
+      // Forms
+      'fieldset','meter','legend','label','input','textarea','form','select',
+      'optgroup','option','output','button','datalist','keygen','progress',
+      // Document sections
+      'body','aside','address','h1','h2','h3','h4','h5','h6','section','header',
+      'nav','article','footer','hgroup',
+      // Tabular data
+      'col','colgroup','caption','table','tr','td','th','tbody','thead','tfoot',
+      // Interactive elements
+      'menu','command','summary','details',
+      // Embedding content
+      'img','area','map','embed','object','param','source','iframe','canvas',
+      'track','audio','video'
+    ),
     eval: adt({
       _: function(attributes) {
         var el = document.createElement(this._key);
