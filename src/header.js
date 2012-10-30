78d906b16ad6a0fcc8cdcec171c679044fc21b46
Rehno Lindeque <errantkid@gmail.com>
2012-10-30 13:07:52 +0200

    Update the description in the copyright header

--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 /*
- * adt-html.js - Algebraic Data Types for JavaScript
+ * adt-html.js - Treat HTML like any old Algebraic Data Type
  * adt-html.js is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
  * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
  * Use it in combination with https://github.com/rehno-lindeque/adt.js
